b9875c5de84c30595fc8285a760a0b136ae4f6ff
Keyboom <pablohildo@gmail.com>
2018-03-14 19:42:41 -0300

    Corrected query
    The query is missing an apostrophe

--- admin/servidor/LoginClass.php
+++ admin/servidor/LoginClass.php
@@ -8,7 +8,7 @@ class LoginClass{
 		if ($mysqli->connect_errno) {
 			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 		}
-		$resultado = $mysqli->query("SELECT * FROM usuarios WHERE user = ".$usuario." AND pass = '".$senha."'");
+		$resultado = $mysqli->query("SELECT * FROM usuarios WHERE user = '".$usuario."' AND pass = '".$senha."'");
 		if (!$resultado){
 			return FALSE;
 		}
