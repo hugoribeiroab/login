4204dfdbc9bc493ecbd309d1476474615c47bafa
hugoribeiroab <35237082+hugoribeiroab@users.noreply.github.com>
2018-03-14 20:18:00 -0300

    Alterando especificações.

--- admin/servidor/servidor.php
+++ admin/servidor/servidor.php
@@ -1,10 +1,10 @@
 <?php
 class Servidor{
-	private $banco = "loguin";
+	private $banco = "SEU BANCO DE DADOS VAI AQUI";
 	private $port = "3306";
-	private $host = "localhost";
-	private $usuarioServidor = "teste2";
-	private $senhaServidor = "teste2";
+	private $host = "O ENDEREÇO DO SEU HOST VAI AQUI, SE FOR LOCAL, USE 'localhost' ";
+	private $usuarioServidor = "SEU USUARIO DO BANCO DE DADOS VAI AQUI";
+	private $senhaServidor = "SUA SENHA DO BANCO DE DADOS VAI AQUI";
 	
 	public function getBanco(){
 		return $this->banco;
